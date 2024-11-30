1578be5d928db4d4289f3df796737a57bce32642
DafiCecilia <00001098547184sp@al.educacao.sp.gov.br>
2024-11-29 22:24:20 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Maligno',
+    'Quem é o vilão principal em Capitão América: Guerra Civil??',
+    'O Barão Zemo,'
+)
+
+criaCartao(
+    'Salve por meio da destruição',
+    'Qual herói luta contra o Capitão América durante a batalha no aeroporto?',
+    'Homem de Ferro (Tony Stark)'
+)
+
+criaCartao(
+    'Saudade',
+    'Quem faz sua primeira aparição no MCU como o Pantera Negra',
+    'Chadwick Boseman'
+)
+
+criaCartao(
+    'Carne fresca',
+    'Qual personagem assume o papel de "Aranha" no filme??',
+    'O Peter Parker'
+)
